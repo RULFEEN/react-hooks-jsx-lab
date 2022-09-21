d9afc1a2aeac30cd1ff7a0aef8b83b7384cc046a
Victor Mayi <victormay.i.797@gmail.com>
2022-09-21 21:49:53 +0300

    commit tree 4af5a8fb4802a4ee5445e4343c8eae8ffd2f4df3

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>You are either a creator or an observer, which are you?</p>
+    <img src={image} alt="I made this"/>
+  </div>
+  );
 }
 
 export default About;
